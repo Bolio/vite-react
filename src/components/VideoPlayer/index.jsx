4720ff0e1291ad401d5801bd6340b9eb1e177579
Bolio <again.bolio@hotmail.com>
2022-03-15 22:30:52 -0600

    Start working on Videos Actions Sidebar

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,18 +1,17 @@
 import clsx from "clsx";
 import { useState, useRef } from "react";
 import styles from "./styles.module.css";
+import VideoPlayerActions from "./VideoPlayerActions";
 
-const SRC =
-  "https://v16-webapp.tiktok.com/487057ab37214a87930458d8aeb3ac97/622fec33/video/tos/useast2a/tos-useast2a-pve-0037c001-aiso/5b297fea74044e928b69eef028966bb9/?a=1988&br=4168&bt=2084&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3CMnz7ThPnMvDXq&l=20220314193013010189050041174DB589&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MzRobDk6ZnkzOzMzZjczM0ApNmY7PDplNGQ2Nzw2PDg2N2dnLWNicjRvNWlgLS1kMWNzc15fYWFeXjIvYGEtXmNhL2I6Yw%3D%3D&vl=&vr=";
+// const SRC =
+//   "https://v16-webapp.tiktok.com/487057ab37214a87930458d8aeb3ac97/622fec33/video/tos/useast2a/tos-useast2a-pve-0037c001-aiso/5b297fea74044e928b69eef028966bb9/?a=1988&br=4168&bt=2084&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3CMnz7ThPnMvDXq&l=20220314193013010189050041174DB589&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MzRobDk6ZnkzOzMzZjczM0ApNmY7PDplNGQ2Nzw2PDg2N2dnLWNicjRvNWlgLS1kMWNzc15fYWFeXjIvYGEtXmNhL2I6Yw%3D%3D&vl=&vr=";
 
-export default function VideoPlayer({src}) {
+export default function VideoPlayer({ src }) {
   const [playing, setPlaying] = useState(false);
   const video = useRef();
   const handlePlay = () => {
     const { current: videoEl } = video;
-    playing
-      ? videoEl.pause()
-      : videoEl.play();
+    playing ? videoEl.pause() : videoEl.play();
 
     setPlaying(!playing);
   };
@@ -22,7 +21,7 @@ export default function VideoPlayer({src}) {
   });
 
   return (
-    <div onClick={handlePlay}>
+    <div className={styles.wrapper}>
       <video
         // autoPlay
         // muted
@@ -34,6 +33,7 @@ export default function VideoPlayer({src}) {
         src={src}
       />
       <i className={playerClassName} onClick={handlePlay} />
+      <VideoPlayerActions />
     </div>
   );
 }
